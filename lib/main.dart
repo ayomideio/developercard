0036653a6cc33f5b26429568f443513faebeb6b4
ayomideio <gokeayomide.tolu@gmail.com>
2021-11-05 13:23:56 +0100

    ayomideio

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,103 @@
 import 'package:flutter/material.dart';
+import 'package:passwordfield/passwordfield.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp2());
+}
+
+class MyApp2 extends StatefulWidget {
+  @override
+  _MyApp2State createState() => _MyApp2State();
+}
+
+class _MyApp2State extends State<MyApp2> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
-  );
+          backgroundColor: Colors.teal,
+          body: SafeArea(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage("images/myavatar2.heic"),
+              ),
+              Text(
+                'Al-Jaber Properties',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                'www.aljaberproperties.com',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20.0,
+                  color: Colors.teal[100],
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.white,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.person,
+                    color: Colors.teal,
+                  ),
+                  title: TextField(
+                      style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.teal[900],
+                  )),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.lock,
+                    color: Colors.teal,
+                  ),
+                  title: TextField(
+                      style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.teal[900],
+                  )),
+                ),
+              ),
+              Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 55.0),
+                  child: ListTile(
+                    tileColor: Colors.tealAccent,
+                    title: TextButton(
+                        child: Text("Login",
+                            style: TextStyle(
+                              fontFamily: 'Source Sans Pro',
+                              fontSize: 25.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.teal[900],
+                            ))),
+                  )),
+            ],
+          ))),
+    );
+  }
 }
